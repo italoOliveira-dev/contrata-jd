dc7d3a2d384f6d07b837b923e1b20adb5af24e74
Henrique Erinaldo <henrique01-_@hotmail.com.br>
2024-07-09 22:20:14 -0300

    head terminado

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -17,9 +17,10 @@ function Navbar() {
         <div className="flex flex-row gap-[30px]">
           <img src="src\Components\Navigation\assets\icon\Vector.svg" alt="" />
           <img src="src\Components\Navigation\assets\icon\bi_chat-left-dots-fill.svg" alt="" />
-          <div>
+          <div className="flex items-center gap-[12px]">
             <img src="https://s3-alpha-sig.figma.com/img/f3a4/8287/6bf054911a8a1a4cc29afafa2bd70fe8?Expires=1721606400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HOl4z0fxw~OwZCQU94aZ~5HM7cqoMk1SKjh6zGxUrC~Mes4wQsf2l~J2qNnJSnYb-kytbm986mV4fcM7CJZzmM8NkYSj4Rcp5ELQ3~3Tuq1JoGkHr0aRAruWBbFpAOo43kzNPGLIQyn2MibbSsn7gZPNGOTp8232GniR7TtDlwtGbZLHzZkbvXJd1FuzdIdTrZjGNBU5RhPM47maLQOMH1vVgOTqggbhuJPFREGD9TGFIDGizSYvtv9FZ594WfKvZmmLwwQ8CWQMh8~ofHHyLSGJ~obKiZdKzRS3C8PMAECdGup1OAz3occmVNPa3qyU55119AlBzpUVn9DRBTUkkw__" className="w-[42px] h-[42px] ml-[-1px] rounded-full"/>
-            <p>Iago Maciel</p>
+            <p className="font-poppins font-medium text-base" style={{ color: 'rgba(22, 30, 84, 1)', fontSize: '16px', alignItems: 'center' }}>Iago Maciel</p>
+            <img src="src\Components\Navigation\assets\icon\Frame10.svg" alt="" />
           </div>
         </div>
         
