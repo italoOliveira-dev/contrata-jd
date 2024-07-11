f404d540472e1de4b7bf8db9b61085ac79d87199
Jamille <jamille.araujo.freitas2003@gmail.com>
2024-07-11 12:56:09 -0300

    [ADD] recently activity component

---
+++ src/Components/Navigation/Components/RecentyActivity.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const RecentActivity = () => {
+  return (
+    <div className="bg-[#161E54] h-auto w-auto sm:h-[400px] sm:w-[271px] md:h-[258px] md:w-[427px] rounded-lg text-white mt-5">
+      <h1 className="bg-[#1B204A] text-start text-[18px] p-4 pl-5 rounded-lg">Atividade Recente</h1>
+       
+      <div className="p-5">
+        <div className="flex flex-col justify-center align-center gap-[6px] text-start ">
+          <p className="text-[10px] opacity-[60%]">10h40, Sexta-feira, 10 de setembro de 2021</p>
+          <h3 className="font-medium leading-7 text-[18px] pr-[50px]">Você Postou uma Nova Vaga</h3>
+          <p className="font-normal text-[14px] leading-5 opacity-[80%]">
+            Por favor, verifique os requisitos e os termos de trabalho e certifique-se de que está tudo correto.
+          </p>      
+        </div>
+
+        <div className="flex flex-col gap-[38px] text-start mt-2 sm:mt-5 md:flex-row md:items-baseline">
+          <p className="font-normal text-[14px] leading-5">Hoje você realizou 12 atividades</p>
+          <button className="w-[139px] rounded-[4px] p-[7px] bg-[#FF5151]" type="button">Ver Todas</button>
+        </div>
+      </div>    
+    </div>
+  )
+}
+
+export default RecentActivity
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,37 +1,41 @@
+import RecentyActivity from "../../Components/Navigation/Components/RecentyActivity";
 import CardEstatusFuncionario from "./Components/cardEstatusFuncionario";
 
 export default function Dashboard() {
 
-    const estatusFuncionarios = [{
-        id: 1,
-        titulo: "Posição Disponível",
-        quantidade: 24,
-        informacao: "4 Urgências"
-    }, {
-        id: 2,
-        titulo: "Vaga de Emprego",
-        quantidade: 10,
-        informacao: "4 Contratação Ativa"
-    }, {
-        id: 3,
-        titulo: "Novos Funcionários",
-        quantidade: 24,
-        informacao: "4 Departamento"
-    }]
+  const estatusFuncionarios = [{
+    id: 1,
+    titulo: "Posição Disponível",
+    quantidade: 24,
+    informacao: "4 Urgências"
+  }, {
+    id: 2,
+    titulo: "Vaga de Emprego",
+    quantidade: 10,
+    informacao: "4 Contratação Ativa"
+  }, {
+    id: 3,
+    titulo: "Novos Funcionários",
+    quantidade: 24,
+    informacao: "4 Departamento"
+  }]
 
-    return (
-        <div className="pl-[51px] pt-[11px]">
-            <h2 className="text-left mb-2 text-[24px] font-medium text-[#161E54]">
-                Dashboard
-            </h2>
-            <div className="grid grid-cols-3 gap-4">
-                {estatusFuncionarios.map(estatus => (
-                    <CardEstatusFuncionario 
-                        key={estatus.id}
-                        estatusFuncionario={estatus}
-                    />
-                ))}
-            </div>
+  return (
+    <div className="pl-[51px] pt-[11px]">
+      <div>
+        <h2 className="text-left mb-2 text-[24px] font-medium text-[#161E54]">
+          Dashboard
+        </h2>
+        <div className="grid grid-cols-3 gap-4">
+          {estatusFuncionarios.map(estatus => (
+              <CardEstatusFuncionario 
+                key={estatus.id}
+                estatusFuncionario={estatus}
+              />
+          ))}
         </div>
-    );
+      </div>
+      <RecentyActivity />
+    </div>
+  );
 }
